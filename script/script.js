d3b96f16f4e9136706e419d688b8b03cd4b155d0
Wesley Sieiro Takatsu de Araujo <23043537+wesleytakatsu@users.noreply.github.com>
2021-12-16 21:18:40 -0300

    adicionando estilo e script

---
+++ script/script.js
@@ -0,0 +1,43 @@
+
+        
+
+var conhecimento = document.getElementById("conhecimento");
+
+function homestart(){
+    geraiss();
+}
+
+function htmll(){
+    conhecimento.innerHTML =`HTML 5:<br>
+    <p>Meus conhecimentos envolvem as principais tags de HTML 5, estruturas básicas, inclusão de estilo e scripts.</p>
+    <p>O meu conhecimento de HTML 5 é o suficiente para dizer que conheço bem a linguagem e que consigo exercer qualquer serviço com eficiência.</p>
+    `;
+}
+
+function csss(){
+    conhecimento.innerHTML =`CSS`;
+
+}
+function javascriptt(){
+    conhecimento.innerHTML =`JavaScript`;
+}
+function cc(){
+    conhecimento.innerHTML =`C`;
+}
+function cplusplus(){
+    conhecimento.innerHTML =`C++`;
+}
+function javaa(){
+    conhecimento.innerHTML =`Java`;
+}
+function pythonn(){
+    conhecimento.innerHTML =`Python`;
+}
+function sqll(){
+    conhecimento.innerHTML =`SQL`;
+}
+
+function geraiss(){
+    conhecimento.innerHTML =`Meus conhecimentos gerais serão apresentados aqui.`;
+}
+homestart();
