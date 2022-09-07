7f6a602305d6a7b7f625c7a87cfaed1a3352aef8
Wesley Sieiro Takatsu de Araujo <23043537+wesleytakatsu@users.noreply.github.com>
2022-09-07 19:01:40 -0300

    teste

--- scripts/script-conhecimentos.js
+++ scripts/script-conhecimentos.js
@@ -13,6 +13,16 @@ function carregaConhecimento(documento){
     $('#slide').fadeIn(800);
 }
 
+function carregaOracle(nomeCertificado, imagemCertificado){
+    const slideInside = document.getElementById('slideInside');
+    slideInside.innerHTML = "<h2>Infraestrutura em núvem (Oracle OCI):</h2>" +
+    "<p>Tenho um bom conhecimento sobre infraestrutura em núvem.</p>" +
+    "<p>Possuo uma certificação: Oracle Cloud Infrastructure Foundations 2021 Certified Associate.</p>" +
+    "<img src='Pagina-Apresentacao-Pessoal/colecao-de-certificados/jpg/" + imagemCertificado + "' alt='Certificação OCI' style='width: 80%;'>";
+
+    $('#slide').fadeIn(800);
+}
+
 function carregaCertificado(nomeCertificado, imagemCertificado){
 
     const slideInside = document.getElementById('slideInside');
