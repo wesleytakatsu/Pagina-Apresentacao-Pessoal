f54c1fea0de617498e73409cb3e9db3dbf1aba35
Wesley Sieiro Takatsu de Araujo <23043537+wesleytakatsu@users.noreply.github.com>
2022-09-07 19:03:56 -0300

    teste

--- scripts/script-conhecimentos.js
+++ scripts/script-conhecimentos.js
@@ -18,7 +18,7 @@ function carregaOracle(nomeCertificado, imagemCertificado){
     slideInside.innerHTML = "<h2>Infraestrutura em núvem (Oracle OCI):</h2>" +
     "<p>Tenho um bom conhecimento sobre infraestrutura em núvem.</p>" +
     "<p>Possuo uma certificação: Oracle Cloud Infrastructure Foundations 2021 Certified Associate.</p>" +
-    "<img src='Pagina-Apresentacao-Pessoal/colecao-de-certificados/jpg/" + imagemCertificado + "' alt='Certificação OCI' style='width: 80%;'>";
+    "<img src='colecao-de-certificados/jpg/" + imagemCertificado + "' alt='Certificação OCI' style='width: 80%;'>";
 
     $('#slide').fadeIn(800);
 }
@@ -27,7 +27,7 @@ function carregaCertificado(nomeCertificado, imagemCertificado){
 
     const slideInside = document.getElementById('slideInside');
     slideInside.innerHTML = "<h2>" + nomeCertificado + ":</h2>" +
-    "<img src='Pagina-Apresentacao-Pessoal/colecao-de-certificados/jpg/" + imagemCertificado + "' alt='Certificação OCI' style='width: 80%;'>";
+    "<img src='colecao-de-certificados/jpg/" + imagemCertificado + "' alt='Certificação OCI' style='width: 80%;'>";
 
     $('#slide').fadeIn(800);
 }
